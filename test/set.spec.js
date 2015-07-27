0dff6a90be75fe875aceef006ed91e6b2c242a34
DerekTBrown <derek@allderek.com>
2015-07-27 13:02:45 -0400

    Force encapsulation

--- test/set.spec.js
+++ test/set.spec.js
@@ -10,28 +10,28 @@ describe('@set directive shall be preprocessed', function () {
 
   describe('in html', function () {
     it('set and echo variables', function () {
-      input = "a<!-- @set test_variable_html 12345-->\n<!-- @echo test_variable_html -->c";
+      input = "a<!-- @set test_variable_html \"12345\"-->\n<!-- @echo test_variable_html -->c";
       pp.preprocess(input).should.equal("a\n12345c");
     });
   });
 
   describe('in javascript', function () {
     it('set and echo variables', function () {
-      input = "a/* @set test_variable_js 12345*/\n/*@echo test_variable_js */c";
+      input = "a/* @set test_variable_js \"12345\"*/\n/*@echo test_variable_js */c";
       pp.preprocess(input, {}, 'js').should.equal("a\n12345c");
     });
   });
 
   describe('in plain text files', function () {
     it('and resolve and echo variables', function () {
-      input = "a\n@set test_variable_txt 12345\n@echo test_variable_txt \nc";
+      input = "a\n@set test_variable_txt \"12345\"\n@echo test_variable_txt \nc";
       pp.preprocess(input, {}, 'simple').should.equal("a\n\n12345\nc");
     });
   });
 
   describe('in coffeescript', function () {
     it('and resolve and echo variables', function () {
-      input = "a\n# @set test_variable_coffee 12345\n# @echo test_variable_coffee\nc";
+      input = "a\n# @set test_variable_coffee \"12345\"\n# @echo test_variable_coffee\nc";
       pp.preprocess(input, {}, 'coffee').should.equal("a\n\n12345\nc");
     });
   });
