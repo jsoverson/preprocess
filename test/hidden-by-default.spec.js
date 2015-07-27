de6baf17818c075a132b08573addeb9df7796aa6
pomutemu <pomutemu@users.noreply.github.com>
2015-07-27 20:36:29 +0900

    Fix illegal HTML test
    See #29.

--- test/hidden-by-default.spec.js
+++ test/hidden-by-default.spec.js
@@ -17,53 +17,53 @@ describe('hidden by default comment blocks shall be preprocessed', function () {
 
   describe('in html', function () {
     it('and process @if and exec nested @echo', function () {
-      input = "a<!-- @if NODE_ENV=='dev' !> <!-- @echo 'b' !> <!-- @endif -->c";
+      input = "a<!-- @if NODE_ENV=='dev' !> <! @echo 'b' !> <! @endif -->c";
       pp.preprocess(input, {NODE_ENV: 'dev'}, 'html').should.equal("a bc");
     });
 
     it('and process @if and exec nested @exec', function () {
-      input = "a<!-- @if NODE_ENV=='dev' !> <!-- @exec hello('b') !> <!-- @endif -->c";
+      input = "a<!-- @if NODE_ENV=='dev' !> <! @exec hello('b') !> <! @endif -->c";
       pp.preprocess(input, {NODE_ENV: 'dev', hello: helloSpy}, 'html').should.equal("a Hello b!c");
       helloSpy.should.have.been.called.with('b');
     });
 
     it('and process @if and exec nested @include', function () {
-      input = "a<!-- @if NODE_ENV=='dev' !> <!-- @include static.txt !> <!-- @endif -->c";
+      input = "a<!-- @if NODE_ENV=='dev' !> <! @include static.txt !> <! @endif -->c";
       pp.preprocess(input, {NODE_ENV: 'dev', srcDir: 'test/fixtures/include'}, 'html').should.equal("a !bazqux!c");
     });
 
     it('and process @if and exec nested @include-static', function () {
-      input = "a<!-- @if NODE_ENV=='dev' !> <!-- @include-static static.txt !> <!-- @endif -->c";
+      input = "a<!-- @if NODE_ENV=='dev' !> <! @include-static static.txt !> <! @endif -->c";
       pp.preprocess(input, {NODE_ENV: 'dev', srcDir: 'test/fixtures/include'}, 'html').should.equal("a !bazqux!c");
     });
 
     it('and process @if and exec nested @exclude', function () {
-      input = "a<!-- @if NODE_ENV=='dev' !> <!-- @exclude !> b <!-- @endexclude !> <!-- @endif -->c";
+      input = "a<!-- @if NODE_ENV=='dev' !> <! @exclude !> b <! @endexclude !> <! @endif -->c";
       pp.preprocess(input, {NODE_ENV: 'dev'}, 'html').should.equal("ac");
     });
 
     it('and process @if and exec nested @extend', function () {
-      input = "a<!-- @if NODE_ENV=='dev' !> <!-- @extend extend.html !>c<!-- @endextend !> <!-- @endif -->c";
+      input = "a<!-- @if NODE_ENV=='dev' !> <! @extend extend.html !>c<! @endextend !> <! @endif -->c";
       pp.preprocess(input, {NODE_ENV: 'dev', srcDir: 'test/fixtures/extend'}, 'html').should.equal("aacbc");
     });
 
     it('and process @extend and exec nested @if', function () {
-      input = "a<!-- @extend extend.html !> <!-- @if NODE_ENV=='dev' !>c<!-- @endif !> <!-- @endextend -->c";
+      input = "a<!-- @extend extend.html !> <! @if NODE_ENV=='dev' !>c<! @endif !> <! @endextend -->c";
       pp.preprocess(input, {NODE_ENV: 'dev', srcDir: 'test/fixtures/extend'}, 'html').should.equal("aacbc");
     });
 
     it('and process @extend and exec nested @ifdef', function () {
-      input = "a<!-- @extend extend.html !> <!-- @ifdef NODE_ENV !>c<!-- @endif !> <!-- @endextend -->c";
+      input = "a<!-- @extend extend.html !> <! @ifdef NODE_ENV !>c<! @endif !> <! @endextend -->c";
       pp.preprocess(input, {NODE_ENV: 'dev', srcDir: 'test/fixtures/extend'}, 'html').should.equal("aacbc");
     });
 
     it('and process @extend and exec nested @ifndef', function () {
-      input = "a<!-- @extend extend.html !> <!-- @ifndef NODE_ENV !>c<!-- @endif !> <!-- @endextend -->c";
+      input = "a<!-- @extend extend.html !> <! @ifndef NODE_ENV !>c<! @endif !> <! @endextend -->c";
       pp.preprocess(input, {NODE_ENV: 'dev', srcDir: 'test/fixtures/extend'}, 'html').should.equal("aabc");
     });
 
     it('and process @if and exec nested @extend', function () {
-      input = "a<!-- @if NODE_ENV=='dev' !> <!-- @foreach $var in ARR !>$var<!-- @endfor !> <!-- @endif -->c";
+      input = "a<!-- @if NODE_ENV=='dev' !> <! @foreach $var in ARR !>$var<! @endfor !> <! @endif -->c";
       pp.preprocess(input, {NODE_ENV: 'dev', ARR: "['b', 'c']"}, 'html').should.equal("abcc");
     });
   });
